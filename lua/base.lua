6848198f379916510af4c9cf1e343f39730afd70
kiotosi <shilo@firecode.ru>
2023-02-17 12:18:21 +0300

    feat(vscode): add optimization for vscode plugin

--- lua/base.lua
+++ lua/base.lua
@@ -1,10 +1,16 @@
-require('features.theme');
-require('features.tabs');
-require('features.wildmenu');
-require('features.search');
-require('features.clipboard');
-require('features.windows');
-require('features.encoding');
-require('features.hooks');
-require('features.standard-plugins');
-require('features.smart-format');
+if vim.g.vscode then
+  require('features.search');
+  require('features.clipboard');
+  require('features.standard-plugins');
+else
+  require('features.theme');
+  require('features.tabs');
+  require('features.wildmenu');
+  require('features.search');
+  require('features.clipboard');
+  require('features.windows');
+  require('features.encoding');
+  require('features.hooks');
+  require('features.standard-plugins');
+  require('features.smart-format');
+end
