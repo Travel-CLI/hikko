8f799bc4d97bbf539129321ddfc050347d660094
kiotosi <shilo@firecode.ru>
2023-02-17 12:34:17 +0300

    feat(ls): add ls for vue and lua

--- lua/plugins/mason.lua
+++ lua/plugins/mason.lua
@@ -1,14 +1,15 @@
 require("mason").setup()
 require("mason-lspconfig").setup({
   ensure_installed = {
-    "sumneko_lua",   -- LSP for Lua language
-    "tsserver",      -- LSP for typescript and javascript
-    "rust_analyzer", -- LSP for Rust
-    "emmet_ls",      -- LSP for Emmet (Vue, HTML, CSS)
-    "cssls",         -- LSP for CSS
-    "stylelint_lsp", -- Linter for CSS, Sass, Less
-    "dockerls",      -- LSP for docker
-    "pyright",       -- LSP for Python
+    "lua_ls",             -- LSP for Lua language
+    "tsserver",           -- LSP for Typescript and Javascript
+    "rust_analyzer",      -- LSP for Rust
+    "emmet_ls",           -- LSP for Emmet (Vue, HTML, CSS)
+    "cssls",              -- LSP for CSS
+    "stylelint_lsp",      -- Linter for CSS, Sass, Less
+    "dockerls",           -- LSP for Docker
+    "pyright",            -- LSP for Python
+    "volar"               -- LSP for Vue
   }
 });
 
